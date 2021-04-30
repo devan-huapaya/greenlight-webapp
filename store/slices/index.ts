56ed6edf778a97c6abfedfccd23cf2870cb2042b
Devan Huapaya <devan@genui.com>
2021-04-29 17:33:23 -0700

    Removed counter

--- store/slices/index.ts
+++ store/slices/index.ts
@@ -1,11 +1,9 @@
 import authReducer from './auth'
-import counterReducer from './counter'
 import myRequestsReducer from './myRequests'
 import navigatorsReducer from './navigators'
 
 export default {
 	auth: authReducer,
-	counter: counterReducer,
 	myRequests: myRequestsReducer,
 	navigators: navigatorsReducer
 }
